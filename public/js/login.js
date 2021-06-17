b78719ac8a7257ab9962e0c8c30f012cd786c769
Vanessa Maldonado <vbarajas4@gmail.com>
2021-06-16 21:01:33 -0700

    login logout sign up for js

---
+++ public/js/login.js
@@ -0,0 +1,26 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    
+    // Collect values from the login form
+    const username = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (username && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the dashboard page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };   
+  document
+      .querySelector('.login-form')
+      .addEventListener('submit', loginFormHandler); 
