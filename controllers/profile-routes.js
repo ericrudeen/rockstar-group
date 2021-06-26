895acf78647b61905aaf47eb05dacb04f415112a
Eric Rudeen <ericrudeen@gmail.com>
2021-06-26 09:42:39 -0700

    fixing the Match endpoint and index routes

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,62 +1,79 @@
-const router = require('express').Router();
-const { User, Artist, User_Artist, Genre, User_Genre, Match } = require('../models');
+const router = require("express").Router();
+const {
+  User,
+  Artist,
+  User_Artist,
+  Genre,
+  User_Genre,
+  Match,
+} = require("../models");
 
-router.get('/', async (req, res) => {
-    try {
-      const newUser = await User.findByPk(1,{
-        include:[
-           {
-               model:Artist,through:{
-                 User_Artist, attributes:[]
-               },
-           },
-           {
-            model:Genre,through:{
-              User_Genre,attributes:[]
-            },
-        } 
-        ] 
-      });
-      res.status(200).json({ newUser });
-  
+router.get("/", async (req, res) => {
+  try {
+    const newUser = await User.findByPk(1, {
+      include: [
+        {
+          model: Artist,
+          through: {
+            User_Artist,
+            attributes: [],
+          },
+        },
+        {
+          model: Genre,
+          through: {
+            User_Genre,
+            attributes: [],
+          },
+        },
+      ],
+    });
+    res.status(200).json({ newUser });
+
     //   req.session.save(() => {
     //     req.session.userId = newUser.id;
     //     // req.session.email = newUser.email;
     //     // req.session.loggedIn = true;
-  
+
     //     res.json(newUser);
     //   });
-    } catch (err) { 
-    console.log(err)
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
-    } 
-  });
+  }
+});
 
+router.get("/Match", async (req, res) => {
+  try {
+    const matches = await Match.findAll({
+      where: { liker: 1 },
+      attributes: {
+        exclude: ["liker", "likee"],
+      },
+      include: [
+        {
+          model: User,
+          as: "likee_user",
+        },
+      ],
+    });
+    res.status(200).json({ matches });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-  router.get('/Match', async (req, res) => {
-    try {
-      const newUser = await Match.findAll({where:{liker:1}},{
-        include:[
-           {
-              model:User
-           },
-           {
-            model:User
-         }, 
-        ] 
-      });
-      res.status(200).json({ newUser });
-  
     //   req.session.save(() => {
     //     req.session.userId = newUser.id;
     //     // req.session.email = newUser.email;
     //     // req.session.loggedIn = true;
-  
+
     //     res.json(newUser);
     //   });
-    } catch (err) { 
-    console.log(err)
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
-    } 
-  });
-module.exports = router
+  }
+});
+module.exports = router;
