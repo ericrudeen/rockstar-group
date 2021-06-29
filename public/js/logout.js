ce11eadb8ec0046452a148f1fd183fe33d330c11
Vanessa Maldonado <vbarajas4@gmail.com>
2021-06-29 03:17:14 -0700

    debugging login and rendering data into views

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,11 @@
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
+    const response = await fetch('/api/user/logout', {
+      method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     });
   
     if (response.ok) {
+      console.log("show something")
       document.location.replace('/');
     } else {
       alert(response.statusText);
--- seeds/matchData.js
+++ seeds/matchData.js
@@ -5,6 +5,23 @@ const matchdata = [
     liker:1,
     likee:2
   },
+  {
+    liker:3,
+    likee:2
+  },
+  {
+    liker:3,
+    likee:1
+  },
+  {
+    liker:2,
+    likee:3
+  },
+  {
+    liker:2,
+    likee:1
+  },
+
 ];
 
 const matchUser = () => Match.bulkCreate(matchdata);
